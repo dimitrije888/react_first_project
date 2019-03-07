34ceb13bc1276c211e155f61fef415892a87948a
dimitrije888 <dimitrije888@hotmail.com>
2019-03-07 10:58:28 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return ( 
+            <div className = "App"> 
+                <h1> hi im a react app </h1>
+                <p>This is really Working!</p>
+                <Person name="Dimitrije" age="26"/>
+                <Person name="Yoki" age="23" >My hobies: Trcanje</Person>
+                <Person name="tebra" age="21"/>
+            </div> 
+        );
+        // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+    }
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const person = (props) => {
+
+    return <p>Im {props.name} and i am {props.age} years old!</p>
+
+};
+
+export default person;
